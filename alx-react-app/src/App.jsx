49d8177477b243a335f26ab18e3df44505879ac9
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-03 19:43:42 +0300

    js files updated and new files created

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,16 @@
+import Footer from './Footer'
+import MainContent from './MainContent'
+import Header from './Header'
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
-
-
     <>
+      <Header />
+      <MainContent />
+      <Footer />
       <WelcomeMessage />
     </>
   )
