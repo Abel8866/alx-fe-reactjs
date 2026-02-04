7292b64f28dc05b8a4c41ffd638d74e26b2d2e28
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-04 19:18:19 +0300

    5 files are updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,14 @@
-import Footer from './components/Footer'
-import MainContent from './components/MainContent'
-import Header from './components/Header'
-import WelcomeMessage from './components/WelcomeMessage'
-import UserProfile from './components/UserProfile'
+import ProfilePage from './ProfilePage'
+import UserContext from './UserContext'
 import './App.css'
 
 function App() {
+  const userData = { name: 'Jane Doe', email: 'jane.doe@example.com' }
+
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-      <WelcomeMessage />
-      <UserProfile name="Alice" age={25} bio="Loves hiking and photography." />
-    </>
+    <UserContext.Provider value={userData}>
+      <ProfilePage userData={userData} />
+    </UserContext.Provider>
   )
 }
 
