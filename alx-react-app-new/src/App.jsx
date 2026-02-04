fcca4cc82ec6883a86988007fdd8b746f799806f
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-04 18:58:08 +0300

    3 files are updated for the 2nd project

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import MainContent from './components/MainContent'
 import Header from './components/Header'
 import WelcomeMessage from './components/WelcomeMessage'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Footer />
       <WelcomeMessage />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography." />
+      <Counter />
     </>
   )
 }
--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,11 +2,11 @@ function Footer() {
   return (
     <footer
       style={{
-        backgroundColor: 'navy',
-        color: 'white',
-        textAlign: 'center',
-        padding: '12px',
-        marginTop: '16px',
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "12px",
+        marginTop: "16px",
       }}
     >
       <p>© 2023 City Lovers</p>
