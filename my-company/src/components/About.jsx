51ea48e57c4655a4eaff9ee6804302b513aed687
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-04 19:38:12 +0300

    the 4 components have been updated

---
+++ my-company/src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>We build products that help teams move faster.</p>
+    </main>
+  );
+}
---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,12 @@
+export default function Services() {
+  return (
+    <main>
+      <h1>Services</h1>
+      <ul>
+        <li>Consulting</li>
+        <li>Design</li>
+        <li>Development</li>
+      </ul>
+    </main>
+  );
+}
