41d94d0bc1bc7a00d42dd0b03d544a8d84ef06f6
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-04 19:39:43 +0300

    app.jsx have been updated

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Services from './components/Services.jsx'
+import Contact from './components/Contact.jsx'
 
 function App() {
-
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
