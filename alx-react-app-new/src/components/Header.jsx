30e3b6bcfbc99ed503529275f4e38cfdb19a7750
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-04 18:34:54 +0300

    header component updated

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,5 +1,9 @@
-<header>
-  <h1>My Favorite Cities</h1>
-</header>
+function Header() {
+  return (
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <h1>My Favorite Cities</h1>
+    </header>
+  )
+}
 
-export default Header;
+export default Header
