e8d4788b1f9d03be1d5a8dcac59c7797b444f35f
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-06 00:24:18 +0300

    Two files are updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
-    <>
+    <Router>
       <h1>Recipe Sharing App</h1>
       <Routes>
         <Route
@@ -21,7 +26,7 @@ function App() {
         <Route path="/recipes/:id" element={<RecipeDetails />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
