98c2b27ac3a7797571ffcda3f5adeb30a9d55e78
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-12 09:40:40 +0300

    fix: correct import path for peopleSaga in rootSaga

--- movie-db/src/app/store/rootSaga.js
+++ movie-db/src/app/store/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from "redux-saga/effects";
 import { moviesSaga } from "../../features/movies/state/moviesSaga";
-import { peopleSaga } from "../../features/people/state/peopleSaga";
+import { peopleSaga } from "../../features/people/state/PeopleSaga";
 import { movieDetailsSaga } from "../../features/movies/state/movieDetailsSaga";
 import { personDetailsSaga } from "../../features/people/state/personDetailsSaga";
 
