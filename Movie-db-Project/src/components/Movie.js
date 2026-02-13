bf7f5071eda041f8d008895bb689b671bce20137
Abel8866 <abel.endale-ug@aau.edu.et>
2026-02-13 10:26:16 +0300

    refactor: clean up Movie component code for improved readability

--- Movie-db-Project/src/components/Movie.js
+++ Movie-db-Project/src/components/Movie.js
@@ -1,21 +1,23 @@
-import react from 'react'
+import React, { useEffect } from "react";
 
-function Movie () {
-
-    const getMovie = () => {
-        fetch('https://api.themoviedb.org/3/discover/movie?api_key=afc035f8a9382b64084f0d247a29675f').then(res => res.json())
-        .then(json => console.log(json))
-    }
-
-    useEffect(()=> {
-        getMovie()
-    },[])
-
-    return (
-        <div>
-            <h1>Movie</h1>
-        </div>
+function Movie() {
+  const getMovie = () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=afc035f8a9382b64084f0d247a29675f",
     )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      <h1>Movie</h1>
+    </div>
+  );
 }
 
 export default Movie;
